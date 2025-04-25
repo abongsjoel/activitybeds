290803a2f0807e42b9cdba22ca112e607ed68cc3
Joel Chi <abongsjoel@gmail.com>
2025-04-25 12:26:04 +0100

    Add page styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import Booking from "./components/BookingPage";
 import Route from "./components/Route";
 import SideNav from "./components/SideNav";
 
+import { sidebar, main } from "./App.module.css";
+
 function App() {
   return (
-    <>
-      <SideNav />
-      <div>
+    <div>
+      <nav className={sidebar}>
+        <SideNav />
+      </nav>
+      <main className={main}>
         <Route path="/">
           <Booking />
         </Route>
         <Route path="/booking">
           <Booking />
         </Route>
-      </div>
-    </>
+      </main>
+    </div>
   );
 }
 
