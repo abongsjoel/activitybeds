7908c197acba6e85f68e5876d3dffd77a8e6a8e1
Joel Chi <abongsjoel@gmail.com>
2025-04-25 21:58:12 +0100

    Add DropButton component

--- src/Pages/BookingPage/index.jsx
+++ src/Pages/BookingPage/index.jsx
@@ -1,30 +1,20 @@
-import Button from "../../components/Button";
+import PageHeader from "../../components/PageHeader";
 import Icon from "../../components/Icon";
 
 import bookIcon from "../../assets/svg/book.svg";
-import plusIcon from "../../assets/svg/fi_plus.svg";
 
-import {
-  booking,
-  titleContainer,
-  title,
-  header,
-} from "./BookingPage.module.css";
+import { booking, pageContent } from "./BookingPage.module.css";
+import DropButton from "../../components/Buttons/DropButton";
 
 export default function Booking() {
   return (
     <section className={booking}>
-      <header className={header}>
-        <div className={titleContainer}>
-          <Button>
-            <Icon src={bookIcon} alt="Booking Icon" />
-          </Button>
-          <span className={title}>Booking</span>
-        </div>
-        <Button primary>
-          <Icon src={plusIcon} alt="Booking Icon" />
-        </Button>
-      </header>
+      <PageHeader pageTitle="Booking" icon={bookIcon} />
+      <main className={pageContent}>
+        <section>
+          <DropButton />
+        </section>
+      </main>
     </section>
   );
 }
--- src/components/Button/index.jsx
+++
@@ -1,7 +0,0 @@
-import { button, btnPrimary } from "./Button.module.css";
-
-export default function Button({ children, primary }) {
-  const primaryStyle = primary ? btnPrimary : "";
-
-  return <button className={`${button} ${primaryStyle}`}>{children}</button>;
-}
