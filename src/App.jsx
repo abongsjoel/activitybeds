c27067faed31503248192c123e7cf836438df97d
Joel Chi <abongsjoel@gmail.com>
2025-04-25 12:06:40 +0100

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Booking from "./components/BookingPage";
+import Route from "./components/Route";
+import SideNav from "./components/SideNav";
+
 function App() {
-  return <>Booking</>;
+  return (
+    <>
+      <SideNav />
+      <div>
+        <Route path="/">
+          <Booking />
+        </Route>
+        <Route path="/booking">
+          <Booking />
+        </Route>
+      </div>
+    </>
+  );
 }
 
 export default App;
